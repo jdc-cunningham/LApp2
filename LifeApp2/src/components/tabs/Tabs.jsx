a6a4b1b52ee8c56cc730a5d535e06fca41c823cb
Jacob Cunningham <jcun4128@gmail.com>
2022-11-06 11:07:02 -0600

    making progress, working on app tabs

---
+++ LifeApp2/src/components/tabs/Tabs.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useEffect } from 'react';
+import './Tabs.scss';
+
+const AppTabs = (props) => {
+  const { apps, activeApp, setActiveApp } = props;
+
+  // ehh
+  const appTabs = Object.keys(apps).map((appKey, index) => (
+    <div
+      key={index}
+      className={`app__tabs-tab ${apps[appKey] === activeApp ? 'active' : ''}`}
+      onClick={() => setActiveApp(apps[appKey])}
+    >
+      {apps[appKey]}
+    </div>
+  ));
+
+  return (
+    <div className="app__tabs">
+      {appTabs}
+    </div>
+  )
+}
+
+export default AppTabs;
